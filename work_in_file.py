c64450251d02bf6c1f01454076d5aef24538f8a8
AlexanderVin <alexandr.vinitsky@gmail.com>
2020-04-08 14:42:35 +0300

    add_08_04_3

--- work_in_file.py
+++ work_in_file.py
@@ -3,16 +3,19 @@
 # for line in file:
 #     print(line, end='')
 # file.close()
+#########################################################################
+# import io
+#
+# file_name = 'command.txt'
+# file = open(file_name, mode='r', encoding='utf8')
+#
+# file_content = file.read(100)
+# print(file_content)
+#
+# new_position = file.seek(10, io.SEEK_SET)
+#
+# file_content = file.read(100)
+# print((file_content))
 
-import io
+##########################################
 
-file_name = 'command.txt'
-file = open(file_name, mode='r', encoding='utf8')
-
-file_content = file.read(100)
-print(file_content)
-
-new_position = file.seek(10, io.SEEK_SET)
-
-file_content = file.read(100)
-print((file_content))
