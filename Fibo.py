085a584a42b97f8b51714965457d9db5e2aa3c61
AlexanderVin <alexandr.vinitsky@gmail.com>
2020-04-10 16:29:42 +0300

    add fibo

---
+++ Fibo.py
@@ -0,0 +1,18 @@
+def fibo(x):
+    if  x == 0:
+        return 0
+    elif x == 1:
+        return 1
+    else:
+        return fibo(x-1) + fibo(x-2)
+print(fibo(7))
+
+def fibonnaci(n):
+    result = []
+    a,b = 0,1
+    for i in range(n):
+        result.append(a)
+        a,b = b, a+b
+    return result
+
+print(fibonnaci(8))
