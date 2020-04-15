03623125537f63444677fdd02fdd9b1d13439484
AlexanderVin <alexandr.vinitsky@gmail.com>
2020-04-15 13:53:52 +0300

    add_15/04_03

--- decorate.py
+++ decorate.py
@@ -1,4 +1,13 @@
 import sys
+from pprint import pprint
 
+print(sys.executable)
 print(sys.platform)
 print(sys.version)
+print(sys.argv)
+pprint(sys.modules)
+
+# print(type(sys.modules), sys.modules)
+
+for k, v in sys.modules.items():
+    print(k, type(v), v, dir(v))
