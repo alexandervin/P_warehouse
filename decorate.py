091354a9b16de845aa2c4bbe02f4d7182ab3dd98
AlexanderVin <alexandr.vinitsky@gmail.com>
2020-04-15 16:02:48 +0300

    add_15/04_04

--- decorate.py
+++ decorate.py
@@ -1,13 +1,24 @@
 import sys
 from pprint import pprint
 
-print(sys.executable)
-print(sys.platform)
-print(sys.version)
-print(sys.argv)
-pprint(sys.modules)
+# print(sys.executable)
+# print(sys.platform)
+# print(sys.version)
+# print(sys.argv)
+# pprint(sys.modules)
 
 # print(type(sys.modules), sys.modules)
+#
+# for k, v in sys.modules.items():
+#     print(k, type(v), v, dir(v))
+# x = {'a': 1, 'b': 2}
+# y = {'b': 10, 'c': 11}
+# z = {**x, **y}
+# print(z)
+#
+# z = x.copy()
+# z.update(y)
+# print(x)
+# print(z)
 
-for k, v in sys.modules.items():
-    print(k, type(v), v, dir(v))
+from PIL import Image
