9fa400db304bc00785395abefd18898cd872088a
AlexanderVin <alexandr.vinitsky@gmail.com>
2020-04-15 11:27:45 +0300

    add_15/04_01

--- Fibo.py
+++ Fibo.py
@@ -20,26 +20,26 @@ print(fibonnaci(8))
 for value in fibonnaci(8):
     print(value)
 
-# class Fibonacci:
-#     '''Итератор последовательности Фибоначчи до N элементов'''
-#
-#     def __init__(self, n):
-#         self.i, self.a, self.b, self.n = 0,0,1,n
-#
-#     def __iter__(self):
-#         self.i, self.a, self.b = 0, 0, 1
-#         return self
-#
-#     def __next__(self):
-#         self.i += 1
-#         if self.i > 1:
-#             if self.i > self.n:
-#                 raise StopIteration
-#             self.a, self.b = self.b, self.a + self.b
-#             return self.a
-#
-# fib_iter = Fibonacci(n=8)
-# for value in fib_iter:
-#     print(value)
-#
+class Fibonacci:
+    '''Итератор последовательности Фибоначчи до N элементов'''
+
+    def __init__(self, n):
+        self.i, self.a, self.b, self.n = 0,0,1,n
+
+    def __iter__(self):
+        self.i, self.a, self.b = 0, 0, 1
+        return self
+
+    def __next__(self):
+        self.i += 1
+        if self.i > 1:
+            if self.i > self.n:
+                raise StopIteration
+            self.a, self.b = self.b, self.a + self.b
+            return self.a
+
+fib_iter = Fibonacci(n=8)
+for value in fib_iter:
+    print(value)
+
 print(13 in fib_iter)
