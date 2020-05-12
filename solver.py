99036990f7ed7b82b3ca250eb24cf86c2854ad91
AlexanderVin <alexandr.vinitsky@gmail.com>
2020-05-12 09:44:16 +0300

    add_12/05

--- solver.py
+++ solver.py
@@ -1,2 +1,6 @@
-def squere_equition_solver():
-    pass
+def squere_equition_solver(a, b, c):
+    if any(map(
+            lambda v: isinstance(v, (int, float)),
+            (a, b, c)
+    )):
+       raise TypeError
--- test_solver.py
+++ test_solver.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+
 class SquareEquationSolverTestCase(TestCase):
 
     def test_solver_module_exist(self):
@@ -11,5 +12,17 @@ class SquareEquationSolverTestCase(TestCase):
     def test_solver_function_exist(self):
         import solver
         self.assertTrue(
-        hasattr(solver, 'squere_equition_solver')
+            hasattr(solver, 'squere_equition_solver')
         )
+
+    def test_function_accepts_args(self):
+        from solver import squere_equition_solver
+        try:
+            squere_equition_solver(1, 70, 600)
+        except TypeError:
+            self.fail('Function must take 3 args')
+
+    def test_raises_on_invalid_argument_type(self):
+        from solver import squere_equition_solver
+        with self.assertRaises(TypeError):
+            squere_equition_solver(1, [-70], None)
